87f843859c8f68bc79c8fabd4db69ca22bcea23b
LuizFelipePUC <127903323+LuizFelipePUC@users.noreply.github.com>
2023-06-25 15:05:32 -0300

    Add files via upload

---
+++ Categorias de Exercício/CardsAtividadesColetivas.js
@@ -0,0 +1,228 @@
+// Array de objetos com informações dos cards
+      const cards = [
+        {
+          id: 'Atividade Coletiva 1',
+          nome: 'Futebol',
+          descricao: 'Faça um gol.',
+          imagem: '../Img_AtividadesColetivas.Js/Futebol.jpg',
+          tópico1descrição: 'Esporte cujo campo de grama apresenta 105 metros de comprimento por 68 metros de largura, uma padronização da Fifa. Duas equipes compostas por 11 jogadores disputam uma partida em que o objetivo é marcar o máximo possível de gols.',
+          tópico2descrição: 'Alivia o estresse, reduz a ansiedade, melhora o foco, estimula o ciclo social, ajuda no trabalho em equipe, melhora quadros de depressão e contribui para o entendimento de limites.',
+          vídeo: 'https://youtu.be/o7yeMRE37Ko',
+        },
+        {
+          id: 'Atividade Coletiva 2',
+          nome: 'Futebol de Sabão',
+          descricao: 'Cuidado onde pisa.',
+          imagem: '../Img_AtividadesColetivas.Js/Futebol de Sabão.jpg',
+          tópico1descrição: '...',
+          tópico2descrição: '...',
+          vídeo: 'https://youtu.be/o7yeMRE37Ko',
+        },
+        {
+          id: 'Atividade Coletiva 3',
+          nome: 'Basquete',
+          descricao: 'Enterrando na cesta.',
+          imagem: '../Img_AtividadesColetivas.Js/Basquete.jpg',
+          tópico1descrição: 'O basquete é um esporte praticado por dois times de 5 jogadores, cujo objetivo é acertar a bola na cesta da equipe adversária, marcando ponto. A equipe com mais pontos ao final da partida será a vencedora. Um jogo de basquete é divido em 4 partes de 10 minutos, totalizando 40 minutos.',
+          tópico2descrição: 'Estimula uma maior concentração de serotonina na corrente sanguínea, diminuindo os níveis de estresse, melhorando o humor e o sono. Além disso, o esporte ajuda no desenvolvimento de novas habilidades, aprimora os reflexos, auxilia no trabalho em equipe e gera novas experiências relacionais.',
+          vídeo: 'https://youtu.be/o7yeMRE37Ko',
+        },
+
+        {
+            id: 'Atividade Coletiva 4',
+            nome: 'Vólei',
+            descricao: 'Hora de sacar.',
+            imagem: '../Img_AtividadesColetivas.Js/Vólei.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 5',
+            nome: 'Corrida',
+            descricao: 'Saia na frente.',
+            imagem: '../Img_AtividadesColetivas.Js/Corrida.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 6',
+            nome: 'Queimada',
+            descricao: 'Queime e desvie.',
+            imagem: '../Img_AtividadesColetivas.Js/Queimada.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 7',
+            nome: 'Tênis',
+            descricao: 'Aprimore seus reflexos.',
+            imagem: '../Img_AtividadesColetivas.Js/Tênis.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 8',
+            nome: 'Queda de Braço',
+            descricao: 'Aguente Firme.',
+            imagem: '../Img_AtividadesColetivas.Js/Queda de Braço.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 9',
+            nome: 'Pique-Esconde',
+            descricao: 'Ache seus amigos.',
+            imagem: '../Img_AtividadesColetivas.Js/Pique esconde.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 10',
+            nome: 'Amarelinha',
+            descricao: 'Faltam muitas casas.',
+            imagem: '../Img_AtividadesColetivas.Js/Amarelhinha.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 11',
+            nome: 'Coelho sai da toca',
+            descricao: 'Seja rápido.',
+            imagem: '../Img_AtividadesColetivas.Js/Coelho sai da toca.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 12',
+            nome: 'Peteca',
+            descricao: 'Timing.',
+            imagem: '../Img_AtividadesColetivas.Js/Peteca.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 13',
+            nome: 'Trilha',
+            descricao: 'Equilíbrio é Essencial.',
+            imagem: '../Img_AtividadesColetivas.Js/Trilha.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 14',
+            nome: 'PaintBall',
+            descricao: 'Proteja seus olhos.',
+            imagem: '../Img_AtividadesColetivas.Js/PaintBall.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+
+          {
+            id: 'Atividade Coletiva 15',
+            nome: 'Jogos de Dança',
+            descricao: 'Basta dançar.',
+            imagem: '../Img_AtividadesColetivas.Js/Dance Games.jpg',
+            tópico1descrição: '...',
+            tópico2descrição: '...',
+            vídeo: 'https://youtu.be/o7yeMRE37Ko',
+          },
+          
+        // Adicione mais objetos aqui para preencher os outros cards
+      ];
+
+      // Função para preencher os cards
+function preencherCards() {
+  var str = "";
+  cards.forEach((card, index) => {
+    str += `<div class="col-md-3 m-5 rounded-circle">
+    <div class="card" id="card-${index}" data-ambiente="tanto-faz" data-necessidade="não" data-duracao="rodadas" data-exercicio="exercicio1">            
+    <img src="${card.imagem}" alt="Imagem do Card 1">
+    <h3>${card.nome}</h3>
+    <p>${card.descricao}</p>
+    <div class="accordion" id="accordionExample-${index}">
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne-${index}" aria-expanded="false" aria-controls="collapseOne-${index}">
+            Descrição
+          </button>
+        </h2>
+        <div id="collapseOne-${index}" class="accordion-collapse collapse" data-bs-parent="#accordionExample-${index}">
+          <div class="accordion-body">
+            ${card.tópico1descrição}
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo-${index}" aria-expanded="false" aria-controls="collapseTwo-${index}">
+            Benefícios
+          </button>
+        </h2>
+        <div id="collapseTwo-${index}" class="accordion-collapse collapse" data-bs-parent="#accordionExample-${index}">
+          <div class="accordion-body">
+            ${card.tópico2descrição}
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree-${index}" aria-expanded="false" aria-controls="collapseThree-${index}">
+            Vídeo
+          </button>
+        </h2>
+        <div id="collapseThree-${index}" class="accordion-collapse collapse" data-bs-parent="#accordionExample-${index}">
+          <div class="accordion-body">
+          <p><a href="${card.vídeo}" class="link-underline-primary">Explicação do Exercício</a></p>          
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  </div>
+  </div>`;
+
+    // Adiciona o evento de clique aos botões do accordion
+    const accordionButtons = document.querySelectorAll(`#card-${index} .accordion-button`);
+    accordionButtons.forEach((button) => {
+      button.addEventListener('click', function () {
+        // Encontra o conteúdo correspondente ao botão clicado
+        const target = this.getAttribute('data-bs-target');
+        const collapse = document.querySelector(target);
+
+        // Abre ou fecha o conteúdo
+        collapse.classList.toggle('show');
+
+        // Altera o atributo aria-expanded
+        const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+        this.setAttribute('aria-expanded', !expanded);
+      });
+    });
+  });
+
+  document.querySelector("#exibirCards").innerHTML = str;
+}
+
+// Chama a função para preencher os cards quando o DOM estiver carregado
+document.addEventListener('DOMContentLoaded', preencherCards);
