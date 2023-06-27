9f895354bfd5f54fc33bdc1c028ad3b8ffb31b05
bernardoOLV <bernardo.oliveira.1468416@sga.pucminas.br>
2023-06-26 22:37:49 -0300

    tela detalhes e my space

--- receitas_cards_script.js
+++ receitas_cards_script.js
@@ -1,15 +1,29 @@
 function selectCard(identificador) {
   var conteudoDescricao = document.getElementById("descricaoReceita");
-  var getReceita = localStorage.getItem(identificador);
-  console.log(getReceita)
-  var objetoReceita = JSON.parse(getReceita);
+  var receitas = localStorage.getItem("receitas");
+  var objetoReceita = JSON.parse(receitas);
+  var strIngredientes = "";
+  strIngredientes = JSON.stringify(objetoReceita[identificador].ingredientes);
+  var vetIngredientes = strIngredientes.split(",");
+  var textoIngredientes = "";
+  for(let i = 0; i< vetIngredientes.length; i++)
+  {
+    textoIngredientes += vetIngredientes[i];
+    textoIngredientes += "<br>"
+  }
+  textoIngredientes = textoIngredientes.replace(/["]/g, "")
+  textoIngredientes = textoIngredientes.replace(/\[|\]/g, "")
+
+
+
+
   var htmlDescricao = "";
-  /*var cardDescricao =
+  var cardDescricao =
     `<div class="row mt-4">
         <div class="col-12">
           <a class="position-absolute top-0 end-0 m-4" href="receitas.html"><button type="button" class="btn-close"
               aria-label="Close"></button></a>
-          <h1 class="text-center mt-5">${objetoReceita.nome}</h1>
+          <h1 class="text-center mt-5">${objetoReceita[identificador].nome}</h1>
         </div>
 
       </div>
@@ -18,26 +32,26 @@ function selectCard(identificador) {
         <div class="col-12 col-md-4 mb-4">
           <h4 class="text-center">Ingredientes</h4>
           <ul>
-            <li style="list-style-type: circle;">${objetoReceita.ingredientes}</li>
+            <li;">${textoIngredientes}</li>
             
           </ul>
 
           
           <h4 class="text-center mt-5">Tags:</h4>
           <ul class="lh-lg">
-          <li>${objetoReceita.tag}</li>
+          <li>${objetoReceita[identificador].tag}</li>
           </ul>
-          
-          <h4 class="text-center mt-5">Curtidas:</h4>
-          <p class="fs-6 text-center"><i class="fa-solid fa-heart" style="color: #ff0000;"></i> ${objetoReceita.curtidas}</p>
-
+          <div class="text-center">
+            <h4 class="text-center mt-5">Curtidas:</h4>
+            <button type="button" class="btn  text-center"><p class=" mt-2"><i class="fa-solid fa-heart" style="color: #ff0000;"></i>Adicionar aos favoritos</p></button>
+          </div>
         </div>
 
         <div class="col-12 col-md-8 mb-5">
           <div id="Preparo">
             <h4 class="text-center">Modo de Preparo</h4>
             <p>
-              ${objetoReceita.modoPreparo}
+              ${objetoReceita[identificador].modoPreparo}
             </p>
           </div>
 
@@ -46,7 +60,7 @@ function selectCard(identificador) {
       </div>`;
   htmlDescricao += cardDescricao;
   conteudoDescricao.innerHTML = htmlDescricao;
-*/
+
 
 }
 
