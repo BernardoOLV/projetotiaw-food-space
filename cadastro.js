8de6091a9532e2cb4c35e9c168a1f49c2c540f7b
bernardoOLV <bernardo.oliveira.1468416@sga.pucminas.br>
2023-07-03 14:08:46 -0300

    .

--- cadastro.js
+++ cadastro.js
@@ -1,16 +1,21 @@
 function Cadastrar() {
     // Referências aos elementos do formulário
     var nomeInput = document.getElementById('nome');
+    var sobrenomeImput = document.getElementById('sobrenome')
     var emailInput = document.getElementById('email');
     var senhaInput = document.getElementById('senha');
   
     // Atribuindo valores
     var nome = nomeInput.value;
+    var sobrenome = sobrenomeImput.value;
     var email = emailInput.value;
     var senha = senhaInput.value;
+
+    
   
     // Testes
     console.log('Nome:', nome);
+    console.log('Nome:', sobrenome);
     console.log('Email:', email);
     console.log('Senha:', senha);
   
